4b3b9868677e685c3033c9a3d6ae7c891a63a032
intromen <vadruf07@gmail.com>
2024-04-22 16:53:30 +0300

    Initial commit

---
+++ src/test/java/ru/iteco/itecospringcource/ITecoSpringCourceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iteco.itecospringcource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITecoSpringCourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
