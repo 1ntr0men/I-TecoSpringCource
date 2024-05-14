a56bdcd73014dbde052f6682dcc250252d0a7811
1ntr0men <vadruf07@gmail.com>
2024-05-14 09:37:11 +0300

    Вебинар. Spring Boot. JPA. Часть 4

--- src/main/java/ru/iteco/itecospringcource/repository/BankBookRepository.java
+++ src/main/java/ru/iteco/itecospringcource/repository/BankBookRepository.java
@@ -1,6 +1,12 @@
 package ru.iteco.itecospringcource.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import ru.iteco.itecospringcource.model.entity.BankBookEntity;
 import ru.iteco.itecospringcource.model.entity.CurrencyEntity;
 
@@ -14,4 +20,9 @@ public interface BankBookRepository extends JpaRepository<BankBookEntity, Intege
     Optional<BankBookEntity> findByUserIdAndNumberAndCurrency(Integer userId, String number, CurrencyEntity currency);
 
     void deleteAllByUserId(Integer userId);
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("select bbe from BankBookEntity bbe where bbe.number = :id")
+    Optional<BankBookEntity> lockById(@Param("id") Integer id);
+
 }
